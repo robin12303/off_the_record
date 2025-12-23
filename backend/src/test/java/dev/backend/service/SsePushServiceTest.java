c15c4e62d281daf5f05641269ea57e8d5132b37c
ys <yeonseoung19@hanyang.ac.kr>
2025-12-23 13:03:52 +0900

    feat: backend test 단위테스트 추가

--- backend/src/test/java/dev/backend/service/SsePushServiceTest.java
+++ backend/src/test/java/dev/backend/service/SsePushServiceTest.java
@@ -3,6 +3,7 @@ package dev.backend.service;
 import dev.backend.components.SseEmitterMetricsRegistry;
 import dev.backend.components.SseEmitterReadRegistry;
 import dev.backend.dto.KeyEventData;
+import dev.backend.dto.MetricEventData;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -25,14 +26,22 @@ class SsePushServiceTest {
             return null;
         }).when(readRegistry).forEach(eq("M-1"), any());
 
-        int sent = service.broadcastMetricsToMachine("M-1", "keyevent", new KeyEventData(
+        int sent_read = service.broadcastReadToMachine("M-1", "key_event", new KeyEventData(
                 "2025:12:22:54",
                 "ON",
                 "DOWN",
                 "a")
                 );
 
-        assertThat(sent).isEqualTo(1);
+        int sent_matric = service.broadcastMetricsToMachine("M-1", "matric_event", new MetricEventData(
+                "2025:12:22:54",
+                1000L,
+                1730000000L,
+                24L))
+                ;
+
+        assertThat(sent_read).isEqualTo(1);
+        assertThat(sent_matric).isEqualTo(1);
         verify(emitter).send(any(SseEmitter.SseEventBuilder.class));
     }
 }
